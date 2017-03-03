03e11de9eb8fe7dee91e3042f231c8006940f106
Jonathan Burk <jburk@foliage.com>
2017-03-03 14:57:33 -0500

    Added CORS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
+var cors = require('cors');
 var db = require('./db');
 var serverPort = 8080;
 
@@ -25,6 +26,9 @@ var options = {
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
 var spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
 var swaggerDoc = jsyaml.safeLoad(spec);
+
+// Add cors
+app.use(cors());
 
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
