6ac97890983b19ea62bf6c3a747f746cfc52ebec
Jonathan Burk <jburk@foliage.com>
2017-04-06 08:50:09 -0400

    Added config for db settings

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
+var config = require('config');
 var mongo = require('mongodb').MongoClient;
 var db = null;
 
 // Open MongoDB connection
 module.exports.connect = function() {
-  mongo.connect('mongodb://localhost:27017/tasklist', function (err, database) {
+  mongo.connect(`mongodb://${config.get('App.db.host')}:${config.get('App.db.port')}/${config.get('App.db.name')}`, function (err, database) {
     if (err) return console.log(err);
     db = database;
   })
